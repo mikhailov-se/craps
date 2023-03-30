1f457e1d27f17399ce4d57c7ffd623771c317d66
mikhailov.se <mikhailov.se@hotmail.com>
2023-03-30 16:35:09 +0300

    tmp

---
+++ apps/app/vault/Vault/Controllers/VaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vault.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VaultController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public VaultController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("test")]
+    public string Test(string name)
+    {
+        return _configuration.GetValue<string>(name);
+    }
+}
